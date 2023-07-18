23cbe384117d91477e4a79ca332873114687f1a4
Faby <105114784+Fxby16@users.noreply.github.com>
2023-07-18 08:40:19 +0200

    .

--- src/global.cpp
+++ src/global.cpp
@@ -2,26 +2,26 @@
 #include <SDL2/SDL_ttf.h>
 #include <vector>
 
-bool run=true; //variable for the main loop
-bool completed=false; //variable to set when sorting is completed to visualize everything green
-bool fullscreen=false; //variable to check if fullscreen is on or off
-bool loaded=false; //true if the array has been randomized, false if already sorted
-bool menuShowed=false; //true if the menu is currently on screen, false if not
-SDL_Event event; //variable for event poll
-SDL_Window *window=NULL; //main window
-SDL_Renderer *renderer=NULL; //main renderer
-TTF_Font *arial=NULL; //text font
+bool run=true; 
+bool completed=false;
+bool fullscreen=false;
+bool loaded=false; 
+bool menuShowed=false; 
+SDL_Event event; 
+SDL_Window *window=NULL; 
+SDL_Renderer *renderer=NULL; 
+TTF_Font *arial=NULL; 
 SDL_Keycode key;
-SDL_Surface *surfaceSortInfo=NULL; //surface that contains current sorting algorithm info
-SDL_Texture *textureSortInfo=NULL; //texture that contains current sorting algorithm info
-SDL_Rect SortInfo_Rect; //rectangle where textureSortInfo will be rendered
-SDL_Surface *surfaceCommands=NULL; //surface with commands info to show when the sorting is completed
-SDL_Texture *textureCommands=NULL; //texture with commands info to show when the sorting is completed
-SDL_Rect Commands_Rect; //rectangle where textureCommands will be rendered
-SDL_Surface *surfaceMenu=NULL; //surface that contains the menu
-SDL_Texture *textureMenu=NULL; //texture that contains the menu
-SDL_Rect Menu_Rect; //rectangle where textureMenu will be rendered
-SDL_Color White={255,255,255}; //text color
+SDL_Surface *surfaceSortInfo=NULL; 
+SDL_Texture *textureSortInfo=NULL; 
+SDL_Rect SortInfo_Rect; 
+SDL_Surface *surfaceCommands=NULL; 
+SDL_Texture *textureCommands=NULL; 
+SDL_Rect Commands_Rect; 
+SDL_Surface *surfaceMenu=NULL; 
+SDL_Texture *textureMenu=NULL; 
+SDL_Rect Menu_Rect; 
+SDL_Color White={255,255,255}; 
 
-int WIDTH=1280,HEIGHT=720; //current size of the window
-std::vector<int>v; //vector for data
+int WIDTH=1280,HEIGHT=720; 
+std::vector<int>v; 
